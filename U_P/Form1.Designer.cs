9d948b68285900803e437575bacabda2adde2166
А <alexander.v_2010@mail.ru>
2018-07-23 13:07:01 +0300

    start development

--- U_P/Form1.Designer.cs
+++ U_P/Form1.Designer.cs
@@ -37,7 +37,7 @@
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(174, 59);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "Branch_mas";
+			this.button1.Text = "Branch_dev";
 			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// Form1
